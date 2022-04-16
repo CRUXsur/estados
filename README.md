64d09c2812aa1468d5686a378b78ef7563a5f7a3
CRUXsur <marcojtorrico@hotmail.com>
2022-04-15 22:03:11 -0400

    Manejadores de Estado - Singleton - Inicio de Proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+# estados
+
+    Manejadores de Estado - Singleton
---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Pagina2')),
+      ),
+      body: Center(
+        child: Text('Pagina2Page'),
+      ),
+    );
+  }
+}
