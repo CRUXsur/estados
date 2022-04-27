f49297d09f953bfac4e9a3e176aa99eb41bb7355
CRUXsur <marcojtorrico@hotmail.com>
2022-04-26 20:44:54 -0400

    BLoC: Mostrar informacion del usuario

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:estados/bloc/user/user_bloc.dart';
+import 'package:estados/models/user.dart';
 
 class Pagina1Page extends StatelessWidget {
   const Pagina1Page({Key? key}) : super(key: key);
@@ -17,9 +18,11 @@ class Pagina1Page extends StatelessWidget {
       //redibujando esta informacion
       body: BlocBuilder<UserBloc, UserState>(
         builder: (_, state) {
-          //buildWhen:
+          //buildWhen: si el nombre del usuario cambia....
           return state.existUser
-              ? const InformacionUsuario()
+              ? InformacionUsuario(
+                  user: state.user!,
+                )
               : const Center(
                   child: Text('No hay usuario seleccionado'),
                 );
@@ -36,8 +39,11 @@ class Pagina1Page extends StatelessWidget {
 }
 
 class InformacionUsuario extends StatelessWidget {
+  final User user;
+
   const InformacionUsuario({
     Key? key,
+    required this.user,
   }) : super(key: key);
 
   @override
@@ -48,22 +54,25 @@ class InformacionUsuario extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text(
+        children: [
+          const Text(
             'General',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-          Divider(),
-          ListTile(title: Text('Nombre:')),
-          ListTile(title: Text('Edad:')),
-          Text(
+          const Divider(),
+          ListTile(title: Text('Nombre:${user.nombre}')),
+          ListTile(title: Text('Edad:${user.edad}')),
+          const Text(
             'Profesiones',
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-          Divider(),
-          ListTile(title: Text('Prefesiones1')),
-          ListTile(title: Text('Profesiones1')),
-          ListTile(title: Text('Profesiones1')),
+          const Divider(),
+          //
+          ...user.profesiones
+              .map((prof) => ListTile(title: Text(prof)))
+              .toList()
+
+          //
         ],
       ),
     );
